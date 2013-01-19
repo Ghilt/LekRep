31447b45e844ad5a6dfbe5c6444dbca95df9ae7d
Marie <ada09mve@student.lth.se>
2013-01-19 12:43:21 +0100

    Bao

--- Lek/src/test/Play.java
+++ Lek/src/test/Play.java
@@ -1,11 +1,15 @@
 package test;
 
 public class Play {
+	private Bao b;
 	
 	public Play() {
 
 		System.out.println("let's DANCE! whogogigigi bogiei");
-
+	}
+	
+	public void setBao(Bao b){
+		this.b=b;
 	}
 
 }
