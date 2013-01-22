a5c782b40fde3d2c9fa85638372d6428afedc438
dt09an1 <dt09an1@mars-10.student.lth.se>
2013-01-22 13:18:45 +0100

    added test without updated classpath

---
+++ Lek/src/realtests/TestPlay.java
@@ -0,0 +1,15 @@
+package realtests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.omg.PortableInterceptor.SUCCESSFUL;
+
+public class TestPlay {
+
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+	}
+
+}
